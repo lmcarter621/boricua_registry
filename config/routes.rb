2ee2b2a15d0e6418e2ab731bb37a05edfd5fd924
Lauren M Carter <lmcarter621@gmail.com>
2017-09-22 20:44:27 -0700

    index page and webhook
    put the body
    
    params as a hash
    
    specs
    
    remove old test framework
    
    remove old test framework
    
    add rspec gem
    
    test reply
    
    working webhook?
    
    skip verify authenticity_token
    
    remove skipping the authenticity token
    
    disable csrf
    
    no csrf token required
    
    skip csrf
    
    remove logs
    
    working webhook
    
    remove tmp directory
    
    updated gitignore
    
    remove secrets
    
    remove individual tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resource :found_people
+
+  root "registry#index"
+  match "twilio_webhook", to: "registry#twilio_webhook", via: [:post]
+
 end
