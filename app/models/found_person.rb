05497938d65cbdfd97c45f28f056c2aa5b819532
Lauren M Carter <lmcarter621@gmail.com>
2017-09-23 16:31:09 -0700

    allow nil phone numbers

--- app/models/found_person.rb
+++ app/models/found_person.rb
@@ -2,7 +2,7 @@ class FoundPerson < ActiveRecord::Base
   before_validation :clean_phone_number
 
   validates :first_name, :last_name, :birthday, :location, presence: true
-  validates :phone_number, :numericality => true, :length => { :minimum => 7, :maximum => 10 }
+  validates :phone_number, :numericality => true, :length => { :minimum => 7, :maximum => 10 }, allow_nil: true
 
   def clean_phone_number
     phone_number&.gsub!(/[^\D]/, '')
