4b78c01ebc1dac0805dc1057268479e6b74712b6
Matt Pasquini <matt.pasquini@coupa.com>
2017-09-22 22:53:20 -0700

    Basic Search

--- app/controllers/found_people_controller.rb
+++ app/controllers/found_people_controller.rb
@@ -1,5 +1,18 @@
 class FoundPeopleController < ApplicationController
   def show
+    @old_search_params = {
+      first_name: params['first_name'],
+      last_name: params['last_name'],
+      location: params['location'],
+      birthday: params['birthday'],
+      status: params['status'],
+    }
+
     @people = FoundPerson.all
+    @people = @people.where('first_name like ?', "#{params['first_name']}%") if params['first_name'].present?
+    @people = @people.where('last_name like ?', "#{params['last_name']}%") if params['last_name'].present?
+    @people = @people.where('location like ?', "#{params['location']}") if params['location'].present?
+    @people = @people.where('birthday = ?', params['birthday'].to_datetime) if params['birthday'].present?
+    @people = @people.where('status like ?', "#{params['status']}") if params['status'].present?
   end
 end
